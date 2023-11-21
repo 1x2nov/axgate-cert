09da3131a92a0ba8cdb691e2d67121690696d0b4
Name <Email>
2023-11-21 15:32:36 +0900

    Snort Automation error fix

--- snort_automation.py
+++ snort_automation.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 # 실행할 명령어
 snort = "snort -A console -r"  #snort 수행 결과 출력
-path_store = '/snort/aegisc_CVE_pcap' #snort로 검증할 pcap이 있는 경로
+path_store = '/snort/aegis_CVE_pcap' #snort로 검증할 pcap이 있는 경로
 path_snort_conf= '/etc/snort.conf' #Snort 설정파일 경로
 path_snort_rule = '/etc/snort/rules/vulnerability.rules' #Snort Rule 파일. 추후 git으로 
 
