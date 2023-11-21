3123361b691a3d8e2418fe5e162c690600fefba2
Name <Email>
2023-11-21 11:27:24 +0900

    File Extract fix

--- file_extract.py
+++ file_extract.py
@@ -1,32 +1,35 @@
-import glob
 import os
-import pandas as pd
 
 path_extract = '/snort/tmp_pcap/'
 path_store = '/snort/aegis_CVE_pcap'
-list_csv='/snort/pcaplist.csv'
 
 
 folders = os.listdir(path_extract)
-pcap_list = pd.read_csv(list_csv)
-filename = pcap_list.loc[:,"File name"]
-listofpcap = filename.values+'.pcap'
-foldername =list("aegis_"+filename.values+'_pcap')
+pcap_list=[]
 
 
 for name in folders :
-    if 'Payload' in name :
-       path_from =path_extract+"/"+name
-       pcapname = os.listdir(path_from)
-       filenum = path_from[-1]
-       for i in range(0,len(foldername)) :
-            if not os.path.exists(path_store+"/"+foldername[i]) :
-                os.makedirs(path_store+"/"+foldername[i])
-            else:
+    if "Payload" in name :
+        pcap_name=os.listdir(path_extract+'/'+name)
+        pcap_list.append([name,pcap_name]) 
+
+for makedir in range(0,len(pcap_list)) :
+    path_from = path_extract+'/'+pcap_list[makedir][0]
+    for namecheck in pcap_list[makedir][1] :
+        if '.pcap' in namecheck :
+            namecheck = namecheck.rstrip('.pcap')
+            namecheck = namecheck.replace(" ","_")
+            namecheck = 'aegis_'+namecheck+'_pcap'
+            if not os.path.exists(path_store+'/'+namecheck) :
+                os.makedirs(path_store+'/'+namecheck)
+            else :
                 continue
-       for pcap in listofpcap: 
-         ext_path, ext_file = os.path.splitext(pcap)
-         if os.path.isfile(path_from+"/"+pcap) :
-           os.rename(path_from+"/"+pcap, path_store+"/"+"aegis_"+ext_path+"_pcap"+"/"+ext_path+"_"+filenum+ext_file)
 
-
+for count in range(0,len(pcap_list)) : 
+    for movepcap in pcap_list[count][1] :
+        if '.pcap' in movepcap :
+            pcapcheck = movepcap.rstrip('.pcap')
+            pcapcheck = pcapcheck.replace(" ","_") 
+        
+            if os.path.exists(path_store+"/"+'aegis_'+pcapcheck+'_pcap') :
+               os.rename(path_extract+'/'+pcap_list[count][0]+'/'+movepcap, path_store+"/"+'aegis_'+pcapcheck+'_pcap'+"/"+pcapcheck+"_"+str(count+1)+".pcap")
