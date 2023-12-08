c9e15e887c0ca709d4fe50cdb289704f4411c5a7
Name <Email>
2023-12-08 14:23:50 +0900

    Snort docker upadte

---
+++ snort_docker/release.sh
@@ -0,0 +1,25 @@
+set -ex
+# SET THE FOLLOWING VARIABLES
+# docker hub username
+USERNAME=dnif
+# image name
+IMAGE=snort
+# ensure we're up to date
+git pull
+# bump version
+echo "$(cat VERSION)" | echo $(awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print }') > VERSION
+
+version=`cat VERSION`
+echo "version: $version"
+# run build
+./build.sh
+# tag it
+git add -A
+git commit -m "version $version"
+git tag -a "$version" -m "version $version"
+git push
+git push --tags
+docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version
+# push it
+docker push $USERNAME/$IMAGE:latest
+docker push $USERNAME/$IMAGE:$version
