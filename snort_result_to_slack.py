f9d214445d0cf05587c84f445faeeba8aa253fa2
Name <Email>
2023-12-11 16:30:08 +0900

    snort conf

---
+++ snort_result_to_slack.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import slack_sdk
+
+
+slack_token = 'xoxb-2210375757591-6218004752135-ub4RV1pRdXgjpoHmkplLNTfM'
+client = slack_sdk.WebClient(token=slack_token)
+result = pd.read_csv('/actions-runner/_work/axgate-cert/axgate-cert/snort_result.csv')
+result = result.values.tolist()
+
+format_message = []
+for index in range(0,len(result)) :
+    format_message.append("취약점 명 : {} 성공 : {} 실패 : {}".format(result[index][0], result[index][1], result[index][2]))
+
+
+for message in format_message :
+    client.chat_postMessage(channel='#snort-auto', text=message)
